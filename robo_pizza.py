e0c150fba3103d8ee00ece340b68cc25075900a4
Jonathan Routley <jonathan.wei.liang@gmail.com>
2022-06-29 11:08:52 +0100

    added larger sprite and created simple update/draw functions that let the user use left/right arrow keys to move robot

--- robo_pizza.py
+++ robo_pizza.py
@@ -3,17 +3,22 @@ import pyxel
 class App:
 
     def __init__(self):
-        pyxel.init(160, 120, title="Robo Pizza")
+        pyxel.init(172, 128, title="Robo Pizza")
         pyxel.load("robo_pizza.py.pyxres")
         self.score = 0
         self.player_x = 80
-        self.player_y = 0
+        self.player_y = 92
+        self.player_dx = 0
+        self.is_alive = True
 
+        # music playm
         pyxel.run(self.update, self.draw)
 
-    def update():
+    def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+
+        self.update_player()
 
 
     def update_pizza():
@@ -22,19 +27,36 @@ class App:
     def update_ingredient():
         pass
 
-    def update_player():
-        pass
+    def update_player(self):
 
-    def draw():
-        pyxel.cls(0)
-        pyxel.rect(10, 10, 20, 20, 11)
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.player_x = max(self.player_x - 2, -8)
+            self.player_dx = -1
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.player_x = min(self.player_x + 2, pyxel.width - 24)
+            self.player_dx = 1
 
+
+
+    def draw(self):
+        pyxel.cls(1)
 
         # draw background
+        pyxel.blt(0, 0, 0, 0, 16, 172, 128)
 
         # draw ingredient
 
         # draw player
+        pyxel.blt(
+            self.player_x,
+            self.player_y,
+            0,
+            0 if self.player_dx < 0 else 32, 
+            160,
+            32,
+            32,
+            1
+        )
 
         # draw score
 
