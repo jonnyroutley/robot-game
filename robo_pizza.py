af91782a3d576007231e84af69fbc3f0109606e5
Jonathan Routley <jonathan.wei.liang@gmail.com>
2022-06-29 17:56:05 +0100

    made function to generate new ingredients

--- robo_pizza.py
+++ robo_pizza.py
@@ -1,15 +1,27 @@
 import pyxel
 
+# GLOBAL VARS
+
+
+
 class App:
 
     def __init__(self):
         pyxel.init(192, 128, title="Robo Pizza")
         pyxel.load("robo_pizza.py.pyxres")
         self.score = 0
-        self.player_x = 80
+        self.player_x = 85
         self.player_y = 92
         self.player_dx = 0
         self.is_alive = True
+
+        # ingredient = (x, y, kind, is_alive)
+        # when the game starts, generate some ingredients that are spread horizontally and vertically
+        num_ingredients = 4
+        self.ingredients = [
+            self.generate_ingredient() for i in range(num_ingredients)
+        ] 
+
 
         # music playm
         pyxel.run(self.update, self.draw)
@@ -20,12 +32,28 @@ class App:
 
         self.update_player()
 
+        for i, v in enumerate(self.ingredients):
+            self.ingredients[i] = self.update_ingredient(*v)
+
 
     def update_pizza():
         pass
 
-    def update_ingredient():
-        pass
+    def generate_ingredient(self):
+        num_types = 9
+        return (pyxel.rndi(8, pyxel.width-8), pyxel.rndi(-70, -10), pyxel.rndi(0, num_types), True)
+
+
+    def update_ingredient(self, x, y, kind, is_alive):
+        # need some terminal condition
+        # if is_alive and abs()
+
+        y += 1
+
+        if y > 100:
+            x, y, kind, is_alive = self.generate_ingredient()
+
+        return (x, y, kind, is_alive)
 
     def update_player(self):
 
@@ -44,7 +72,12 @@ class App:
         # draw background
         pyxel.blt(0, 0, 0, 0, 16, 192, 128)
 
-        # draw ingredient
+        # draw ingredients
+        for x, y, kind, is_alive in self.ingredients:
+            if is_alive:
+                pyxel.blt(x, y, 1, (kind % 4) * 16, pyxel.floor(kind / 4) * 16, 16, 16, 1)
+
+
 
         # draw player
         pyxel.blt(
